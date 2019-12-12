813846562226f94ef5df58eec43f7e64af052250
Matthew Herman <48136613+mhermandesign@users.noreply.github.com>
2019-12-12 14:36:15 -0600

    Refactoring and Fixing
    Removed a bunch of broken code, fixed some wording, and reduced the overall amount of code by removing some redundancies.

--- js/main.js
+++ js/main.js
@@ -1,104 +1,108 @@
 let d = new Date();
 let thisHour = d.getHours();
-
-//it is pm if hours from 12 onwards
-let suffix = (thisHour >= 12) ? 'pm' : 'am';
-//only -12 from hours if it is greater than 12 (if not back at mid night)
-thisHour = (thisHour > 12) ? thisHour - 12 : thisHour;
-//if 00 then it is 12 am
-thisHour = (thisHour == '00') ? 12 : thisHour;
-
-const weekday = [{
-        day: 'Sunday',
-        opening: 8,
-        closing: 8
-    },
-    {
-        day: 'Monday',
-        opening: 6.5,
-        closing: 10
-    },
-    {
-        day: 'Tuesday',
-        opening: 6.5,
-        closing: 10
-    },
-    {
-        day: 'Wednesday',
-        opening: 6.5,
-        closing: 10
-    },
-    {
-        day: 'Thursday',
-        opening: 6.5,
-        closing: 10
-    },
-    {
-        day: 'Friday',
-        opening: 6.5,
-        closing: 10
-    },
-    {
-        day: 'Saturday',
-        opening: 6.5,
-        closing: 10
-    }
-]
-
-let today = weekday[d.getDay()];
-let tomorrow = weekday[d.getDay() + 1];
-
-console.log(today.day);
-console.log(tomorrow.day);
-console.log(thisHour)
+let isOpen;
 
 const response = document.querySelector('#response');
 const storeHours = document.querySelector('#store-hours');
 
-if (thisHour >= today.opening && thisHour <= today.closing) {
+const weekday = [{
+        day: 'Sunday',
+        opening: 8,
+        closing: 20
+    },
+    {
+        day: 'Monday',
+        opening: 6.5,
+        closing: 22
+    },
+    {
+        day: 'Tuesday',
+        opening: 6.5,
+        closing: 22
+    },
+    {
+        day: 'Wednesday',
+        opening: 6.5,
+        closing: 22
+    },
+    {
+        day: 'Thursday',
+        opening: 6.5,
+        closing: 22
+    },
+    {
+        day: 'Friday',
+        opening: 6.5,
+        closing: 22
+    },
+    {
+        day: 'Saturday',
+        opening: 6.5,
+        closing: 22
+    }
+]
+
+// Get index of today and tomorrow
+let today = weekday[d.getDay()];
+let tomorrow = weekday[d.getDay() + 1];
+
+// Check if open
+if (thisHour > today.opening && thisHour < today.closing) {
     isOpen = true;
 } else {
     isOpen = false;
 }
 
+let todayOpening;
+let tomorrowOpening
+let todayClosing;
+let tomorrowClosing;
+
+// Set todayOpening
 if (today.opening === 6.5) {
-    today.opening = '6:30';
+    todayOpening = '6:30';
 } else if (today.opening === 8) {
-    today.opening = '8:00'
+    todayOpening = '8:00';
 } else {
-    today.opening = '!!!Something Broke!!!'
+    alert('ERROR: Today Opening');
 }
 
-if (today.closing === 8) {
-    today.closing = '8:00';
-} else if (today.closing === 10) {
-    today.closing = '10:00';
+// Set todayClosing
+if (today.closing === 20) {
+    todayClosing = '8:00';
+} else if (today.closing === 22) {
+    todayClosing = '10:00';
 } else {
-    today.opening = '!!!Something Broke!!!'
+    alert('ERROR: Today Closing');
 }
 
+// Set tomorrowOpening
 if (tomorrow.opening === 6.5) {
-    tomorrow.opening = '6:30';
+    tomorrowOpening = '6:30';
 } else if (tomorrow.opening === 8) {
-    tomorrow.opening = '8:00'
+    tomorrowOpening = '8:00';
 } else {
-    tomorrow.opening = '!!!Something Broke!!!'
+    alert('ERROR: Tomorrow Opening');
 }
 
-if (tomorrow.closing === 8) {
-    tomorrow.closing = '8:00';
-} else if (tomorrow.closing === 10) {
-    tomorrow.closing = '10:00';
+// Set tomorrowClosing
+if (tomorrow.closing === 20) {
+    tomorrowClosing = '8:00';
+} else if (tomorrow.closing === 22) {
+    tomorrowClosing = '10:00';
 } else {
-    tomorrow.opening = '!!!Something Broke!!!'
+    alert('ERROR: Tomorrow Closing');
 }
 
+let untilClosing = today.closing - thisHour;
+
+// Render
 if (isOpen) {
     response.src = 'images/YES.png';
     response.alt = 'YES!';
-    storeHours.textContent = `Open in Ames, IA today from ${today.opening}AM until ${today.closing}PM`;
+    storeHours.textContent = `Menards is open in Ames, IA today from ${todayOpening}AM until ${todayClosing}PM. The store is open for, roughly, another ${untilClosing > 1 ? untilClosing + ' hours' : 'hour'}.`;
 } else {
     response.src = 'images/NO.png';
     response.alt = 'NO!';
-    storeHours.textContent = `Open in Ames, IA tomorrow from ${tomorrow.opening}AM until ${tomorrow.closing}PM`;
+    storeHours.textContent = `Menards is open in Ames, IA tomorrow from ${tomorrowOpening}AM until ${tomorrowClosing}PM.`;
 }
